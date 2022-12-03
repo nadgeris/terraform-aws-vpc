ee9b53f18908a1335877b06724993f19eff384c6
Sagar Nadgeri <sagarnadgeri@ip-192-168-2-36.us-west-1.compute.internal>
2022-12-03 09:17:47 -0500

    updates

--- outputs.tf
+++ outputs.tf
@@ -1,46 +1,37 @@
 output "id" {
   value       = aws_vpc.default.id
-  description = "VPC ID"
 }
 
 output "app_public_subnet_ids" {
   value       = aws_subnet.app-public.*.id
-  description = "List of public subnet IDs"
 }
 
 output "rds_public_subnet_ids" {
   value       = aws_subnet.rds-public.*.id
-  description = "List of public subnet IDs"
 }
 
 output "monitor_public_subnet_ids" {
   value       = aws_subnet.monitor-public.*.id
-  description = "List of public subnet IDs"
 }
 
 output "app_private_subnet_ids" {
   value       = aws_subnet.app-private.*.id
-  description = "List of private subnet IDs"
 }
 
 output "rds_private_subnet_ids" {
   value       = aws_subnet.rds-private.*.id
-  description = "List of private subnet IDs"
 }
 
 output "monitor_private_subnet_ids" {
   value       = aws_subnet.monitor-private.*.id
-  description = "List of private subnet IDs"
 }
 
 output "cidr_block" {
   value       = var.cidr_block
-  description = "The CIDR block associated with the VPC"
 }
 
 output "nat_gateway_ips" {
   value       = aws_eip.nat.*.public_ip
-  description = "List of Elastic IPs associated with NAT gateways"
 }
 
 output "private_route_table" {
@@ -49,4 +40,8 @@ output "private_route_table" {
 
 output "public_route_table" {
   value       = aws_route_table.public.id
+}
+
+output "vpc_cidr" {
+  value       = local.cidr_block
 }
