710deaf4db29d99bb0180cedbf9b3c9e251af73a
Sagar Nadgeri <sagarnadgeri@ip-192-168-2-36.us-west-1.compute.internal>
2022-12-02 21:34:14 -0500

    changes

--- main.tf
+++ main.tf
@@ -4,9 +4,12 @@ data "aws_availability_zones" "available" {
 
 locals {
   cidr_block = "172.${var.second_octet}.0.0/16"
-  name = ["app", "rds", "monitoring"]
-  private_subnets = ["172.${var.second_octet}.1.0/24", "172.${var.second_octet}.2.0/24", "172.${var.second_octet}.3.0/24"]
-  public_subnets = ["172.${var.second_octet}.11.0/24", "172.${var.second_octet}.12.0/24", "172.${var.second_octet}.13.0/24"]
+  ap_private_subnets = ["172.${var.second_octet}.1.0/24", "172.${var.second_octet}.2.0/24", "172.${var.second_octet}.3.0/24"]
+  rds_private_subnets = ["172.${var.second_octet}.11.0/24", "172.${var.second_octet}.12.0/24", "172.${var.second_octet}.13.0/24"]
+  monitor_private_subnets = ["172.${var.second_octet}.21.0/24", "172.${var.second_octet}.22.0/24", "172.${var.second_octet}.23.0/24"]
+  app_public_subnets = ["172.${var.second_octet}.101.0/24", "172.${var.second_octet}.102.0/24", "172.${var.second_octet}.103.0/24"]
+  rds_public_subnets = ["172.${var.second_octet}.111.0/24", "172.${var.second_octet}.112.0/24", "172.${var.second_octet}.113.0/24"]
+  monitor_public_subnets = ["172.${var.second_octet}.121.0/24", "172.${var.second_octet}.122.0/24", "172.${var.second_octet}.123.0/24"]
 }
 
 #
@@ -41,7 +44,7 @@ resource "aws_internet_gateway" "default" {
 }
 
 resource "aws_route_table" "private" {
-  count = length(local.private_subnets) > 0 ? 1: 0
+  count = length(local.ap_private_subnets) > 0 ? 1: 0
 
   vpc_id = aws_vpc.default.id
 
@@ -80,15 +83,15 @@ resource "aws_route" "public" {
   gateway_id             = aws_internet_gateway.default.id
 }
 
-resource "aws_subnet" "private" {
-  count = length(data.aws_availability_zones.available.names) < 3 ? length(data.aws_availability_zones.available.names) : 3
+resource "aws_subnet" "app-private" {
+  count = length(local.ap_private_subnets)
   vpc_id            = aws_vpc.default.id
-  cidr_block        = local.private_subnets[count.index]
+  cidr_block        = local.ap_private_subnets[count.index]
   availability_zone = data.aws_availability_zones.available.names[count.index]
 
   tags = merge(
     {
-      Name        = format("%s-%s-%s-PrivateSubnet", var.name, var.environment, local.name[count.index]),
+      Name        = format("%s-%s-app-PrivateSubnet", var.name, var.environment),
       Environment = var.environment,
       Terraform   = "true",
       type        = "private"
@@ -97,16 +100,51 @@ resource "aws_subnet" "private" {
   )
 }
 
-resource "aws_subnet" "public" {
-  count = length(data.aws_availability_zones.available.names) < 3 ? length(data.aws_availability_zones.available.names) : 3
+resource "aws_subnet" "rds-private" {
+  count = length(local.rds_private_subnets)
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = local.rds_private_subnets[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = merge(
+    {
+      Name        = format("%s-%s-rds-PrivateSubnet", var.name, var.environment),
+      Environment = var.environment,
+      Terraform   = "true",
+      type        = "private"
+    },
+    var.tags
+  )
+}
+
+resource "aws_subnet" "monitor-private" {
+  count = length(local.monitor_private_subnets)
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = local.monitor_private_subnets[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = merge(
+    {
+      Name        = format("%s-%s-monitor-PrivateSubnet", var.name, var.environment),
+      Environment = var.environment,
+      Terraform   = "true",
+      type        = "private"
+    },
+    var.tags
+  )
+}
+
+
+resource "aws_subnet" "app-public" {
+  count = length(local.app_public_subnets)
   vpc_id                  = aws_vpc.default.id
-  cidr_block              = local.public_subnets[count.index]
+  cidr_block              = local.app_public_subnets[count.index]
   availability_zone = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = true
 
   tags = merge(
     {
-      Name        = format("%s-%s-%s-PublicSubnet", var.name, var.environment, local.name[count.index]),
+      Name        = format("%s-%s-app-PublicSubnet", var.name, var.environment),
       Environment = var.environment,
       Terraform   = "true",
       type        = "public"
@@ -115,16 +153,79 @@ resource "aws_subnet" "public" {
   )
 }
 
-resource "aws_route_table_association" "private" {
-  count = length(data.aws_availability_zones.available.names) < 3 ? length(data.aws_availability_zones.available.names) : 3
 
-  subnet_id      = aws_subnet.private[count.index].id
+resource "aws_subnet" "rds-public" {
+  count = length(local.rds_public_subnets)
+  vpc_id                  = aws_vpc.default.id
+  cidr_block              = local.rds_public_subnets[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+
+  tags = merge(
+    {
+      Name        = format("%s-%s-rds-PublicSubnet", var.name, var.environment),
+      Environment = var.environment,
+      Terraform   = "true",
+      type        = "public"
+    },
+    var.tags
+  )
+}
+
+resource "aws_subnet" "monitor-public" {
+  count = length(local.monitor_public_subnets)
+  vpc_id                  = aws_vpc.default.id
+  cidr_block              = local.monitor_public_subnets[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+
+  tags = merge(
+    {
+      Name        = format("%s-%s-monitor-PublicSubnet", var.name, var.environment),
+      Environment = var.environment,
+      Terraform   = "true",
+      type        = "public"
+    },
+    var.tags
+  )
+}
+
+
+resource "aws_route_table_association" "app-private" {
+  count = length(local.ap_private_subnets)
+
+  subnet_id      = aws_subnet.app-private[count.index].id
   route_table_id = aws_route_table.private[0].id
 }
 
-resource "aws_route_table_association" "public" {
-  count = length(data.aws_availability_zones.available.names) < 3 ? length(data.aws_availability_zones.available.names) : 3
-  subnet_id      = aws_subnet.public[count.index].id
+resource "aws_route_table_association" "app-public" {
+  count = length(local.app_public_subnets)
+  subnet_id      = aws_subnet.app-public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "rds-private" {
+  count = length(local.rds_private_subnets)
+  subnet_id      = aws_subnet.rds-private[count.index].id
+  route_table_id = aws_route_table.private[0].id
+}
+
+resource "aws_route_table_association" "rds-public" {
+  count = length(local.rds_public_subnets)
+  subnet_id      = aws_subnet.rds-public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "monitor-private" {
+  count = length(local.monitor_private_subnets)
+
+  subnet_id      = aws_subnet.monitor-private[count.index].id
+  route_table_id = aws_route_table.private[0].id
+}
+
+resource "aws_route_table_association" "monitr-public" {
+  count = length(local.monitor_public_subnets)
+  subnet_id      = aws_subnet.monitor-public[count.index].id
   route_table_id = aws_route_table.public.id
 }
 
@@ -132,7 +233,7 @@ resource "aws_route_table_association" "public" {
 # NAT resources
 #
 resource "aws_eip" "nat" {
-  count = length(local.public_subnets) > 0 ? 1: 0
+  count = length(local.app_public_subnets) > 0 ? 1: 0
 
   vpc = true
 }
@@ -140,10 +241,10 @@ resource "aws_eip" "nat" {
 resource "aws_nat_gateway" "default" {
   depends_on = [aws_internet_gateway.default]
 
-  count = length(local.public_subnets) > 0 ? 1: 0
+  count = length(local.app_public_subnets) > 0 ? 1: 0
 
   allocation_id = aws_eip.nat[0].id
-  subnet_id     = aws_subnet.public[0].id
+  subnet_id     = aws_subnet.app-public[0].id
 
   tags = merge(
     {
--- outputs.tf
+++ outputs.tf
@@ -3,13 +3,33 @@ output "id" {
   description = "VPC ID"
 }
 
-output "public_subnet_ids" {
-  value       = aws_subnet.public.*.id
+output "app_public_subnet_ids" {
+  value       = aws_subnet.app-public.*.id
   description = "List of public subnet IDs"
 }
 
-output "private_subnet_ids" {
-  value       = aws_subnet.private.*.id
+output "rds_public_subnet_ids" {
+  value       = aws_subnet.rds-public.*.id
+  description = "List of public subnet IDs"
+}
+
+output "monitor_public_subnet_ids" {
+  value       = aws_subnet.monitor-public.*.id
+  description = "List of public subnet IDs"
+}
+
+output "app_private_subnet_ids" {
+  value       = aws_subnet.app-private.*.id
+  description = "List of private subnet IDs"
+}
+
+output "rds_private_subnet_ids" {
+  value       = aws_subnet.rds-private.*.id
+  description = "List of private subnet IDs"
+}
+
+output "monitor_private_subnet_ids" {
+  value       = aws_subnet.monitor-private.*.id
   description = "List of private subnet IDs"
 }
 
